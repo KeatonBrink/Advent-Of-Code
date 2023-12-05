071c7b4c44946eb9eb67b04674a610a68c5165de
Keaton Brinkerhoff <keatonbrink@gmail.com>
2023-12-05 08:30:59 -0700

    Add more to day 4, part 1

--- AofC2023/day5/Part1/main.go
+++ AofC2023/day5/Part1/main.go
@@ -4,9 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 	"unicode"
 )
+
+type Pair struct {
+	Destination, SD_Range int
+}
 
 func main() {
 	input_file_name := "input.txt"
@@ -26,31 +31,39 @@ func main() {
 		text = append(text, file_scanner.Text())
 	}
 
-	seeds := strings.Split(text[0], " ")[1:]
+	var seeds []int
+	for _, seed := range strings.Split(text[0], " ")[1:] {
+		seed1, err := strconv.Atoi(seed)
+		if err != nil {
+			panic(err)
+		}
+		seeds = append(seeds, seed1)
+	}
 	text = text[2:]
 
-	first := true
+	// first := true
 
-	var transformers map[string]map[int]int
+	var transformers map[string]map[int]Pair
 
 	var all_categories []string
 
 	var current_category string
 
-	var next_category string
+	// var next_category string
 
 	for _, line := range text {
 		if unicode.IsLetter(rune(line[0])) {
 			split_string := strings.Split(line, "-")
 			current_category = split_string[0]
-			next_category = split_string[2]
-			transformers[current_category] = make(map[int]int)
-			if first {
-				all_categories = append(all_categories, current_category)
-			} else {
-				first = false
-			}
-			all_categories = append(all_categories, next_category)
+			// next_category = strings.Split(split_string[2], " ")[0]
+			// println(next_category)
+			transformers[current_category] = make(map[int]Pair)
+			// if first {
+			all_categories = append(all_categories, current_category)
+			// } else {
+			// first = false
+			// }
+			// all_categories = append(all_categories, next_category)
 		} else if unicode.IsDigit(rune(line[0])) {
 			var source, destination, sd_range int
 			found, err := fmt.Sscanf(line, "%d %d %d", &source, &destination, &sd_range)
@@ -60,6 +73,13 @@ func main() {
 			if found != 3 {
 				fmt.Printf("Found only %d", found)
 			}
+			transformers[current_category][source] = Pair{Destination: destination, SD_Range: sd_range}
 		}
 	}
+
+	// closest_location :=
+
+	// Loop through all the seeds
+	// Loop through the tranformer, until the end
+	// If the "location" is the lowest seen so far, save it
 }
