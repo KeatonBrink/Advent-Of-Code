03d818ffa69d8c88c0813210d59d8f0bce3cfecd
Keaton Brinkerhoff <keatonbrink@gmail.com>
2023-12-13 07:51:30 -0700

    Finish day 13 part 2

--- AofC2023/day13/Part2/main.go
+++ AofC2023/day13/Part2/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bufio"
-	"math"
+	"fmt"
 	"os"
 )
 
@@ -28,93 +28,93 @@ func main() {
 		text = append(text, file_scanner.Text())
 	}
 
-	// Grab galaxies and create new
-	var galaxies []Coordinate
-	var galaxy_image [][]byte
-	for row, line := range text {
-		var line_bytes []byte
-		for col, character := range line {
-			line_bytes = append(line_bytes, byte(character))
-			if character == '#' {
-				galaxies = append(galaxies, Coordinate{Row: row, Col: col})
-			}
-		}
-		galaxy_image = append(galaxy_image, line_bytes)
-	}
+	var reflection_problems [][]string
 
-	// Find any row that has to be repeated
-	double_row := make(map[int]bool)
-	for i_row, row := range galaxy_image {
-		found_galaxy := false
-		for _, char := range row {
-			if char == '#' {
-				found_galaxy = true
-				break
-			}
-		}
-		double_row[i_row] = !found_galaxy
-	}
+	var cur_reflection_problem []string
 
-	// Find any column that has to be repeated
-	double_col := make(map[int]bool)
-	for i_col := 0; i_col < len(galaxy_image[0]); i_col++ {
-		found_galaxy := false
-		for i_row := 0; i_row < len(galaxy_image); i_row++ {
-			if galaxy_image[i_row][i_col] == '#' {
-				found_galaxy = true
-				break
-			}
-		}
-		double_col[i_col] = !found_galaxy
-	}
-
-	total_distance := 0
-
-	// Start Calculating Distances
-	for i_start_galaxy, start_galaxy := range galaxies {
-		for i_other_galaxy := i_start_galaxy + 1; i_other_galaxy < len(galaxies); i_other_galaxy++ {
-			other_galaxy := galaxies[i_other_galaxy]
-			total_distance += int(math.Abs(float64(start_galaxy.Col)-float64(other_galaxy.Col))) + int(math.Abs(float64(start_galaxy.Row)-float64(other_galaxy.Row))) + FindDoubleDistances(start_galaxy, other_galaxy, double_row, double_col)
-
+	for _, line := range text {
+		if line != "" {
+			cur_reflection_problem = append(cur_reflection_problem, line)
+		} else {
+			reflection_problems = append(reflection_problems, cur_reflection_problem)
+			cur_reflection_problem = make([]string, 0)
 		}
 	}
+	reflection_problems = append(reflection_problems, cur_reflection_problem)
 
-	println(total_distance)
+	count := 0
+
+	for _, cur_reflection_problem = range reflection_problems {
+		// PrintReflectionProblem(cur_reflection_problem)
+		count += FindColumnReflection(cur_reflection_problem)
+		count += FindRowReflection(cur_reflection_problem)
+		// println(count)
+	}
+
+	fmt.Printf("Final reflection count: %d\n", count)
 }
 
-func FindDoubleDistances(start_galaxy, other_galaxy Coordinate, double_row, double_col map[int]bool) int {
-	ret_doubles := 0
-	var cur_i int
-	var end_i int
-	if start_galaxy.Row < other_galaxy.Row {
-		cur_i = start_galaxy.Row + 1
-		end_i = other_galaxy.Row - 1
-	} else {
-		cur_i = other_galaxy.Row + 1
-		end_i = start_galaxy.Row - 1
-	}
-
-	for ; cur_i <= end_i; cur_i++ {
-		if double_row[cur_i] {
-			ret_doubles += 1000000 - 1
-			// ret_doubles += 100 - 1
+func FindColumnReflection(cur_reflection_problem []string) int {
+	count := 0
+	for start_right := 1; start_right < len(cur_reflection_problem[0]); start_right++ {
+		difference_count := 0
+		for _, cur_row := range cur_reflection_problem {
+			left := reverse(cur_row[:start_right])
+			right := cur_row[start_right:]
+			for comparator_ind := 0; comparator_ind < len(left) && comparator_ind < len(right); comparator_ind++ {
+				if left[comparator_ind] != right[comparator_ind] {
+					difference_count++
+				}
+			}
+			if difference_count > 1 {
+				break
+			}
+		}
+		if difference_count == 1 {
+			count += start_right
 		}
 	}
+	return count
+}
 
-	if start_galaxy.Col < other_galaxy.Col {
-		cur_i = start_galaxy.Col + 1
-		end_i = other_galaxy.Col - 1
-	} else {
-		cur_i = other_galaxy.Col + 1
-		end_i = start_galaxy.Col - 1
-	}
-
-	for ; cur_i <= end_i; cur_i++ {
-		if double_col[cur_i] {
-			ret_doubles += 1000000 - 1
-			// ret_doubles += 100 - 1
+func FindRowReflection(cur_reflection_problem []string) int {
+	count := 0
+	for start_down := 1; start_down < len(cur_reflection_problem); start_down++ {
+		difference_count := 0
+		for cur_col := 0; cur_col < len(cur_reflection_problem[0]); cur_col++ {
+			//Convert the column to a string for copying from above
+			column_string := ""
+			for i := 0; i < len(cur_reflection_problem); i++ {
+				column_string += string(cur_reflection_problem[i][cur_col])
+			}
+			left := reverse(column_string[:start_down])
+			right := column_string[start_down:]
+			for comparator_ind := 0; comparator_ind < len(left) && comparator_ind < len(right); comparator_ind++ {
+				if left[comparator_ind] != right[comparator_ind] {
+					difference_count++
+				}
+			}
+			if difference_count > 1 {
+				break
+			}
+		}
+		if difference_count == 1 {
+			// fmt.Printf("Valid column %d\n", start_down)
+			count += start_down * 100
 		}
 	}
+	return count
+}
 
-	return ret_doubles
+func reverse(str string) (result string) {
+	for _, v := range str {
+		result = string(v) + result
+	}
+	return
+}
+
+func PrintReflectionProblem(p []string) {
+	for _, str := range p {
+		println(str)
+	}
 }
