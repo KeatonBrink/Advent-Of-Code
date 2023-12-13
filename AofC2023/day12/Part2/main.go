ceebdc7f91c9114ecda9822f616e4108d6bf18da
Keaton Brinkerhoff <keatonbrink@gmail.com>
2023-12-13 08:30:20 -0700

    Finish day 12 part 2

--- AofC2023/day12/Part2/main.go
+++ AofC2023/day12/Part2/main.go
@@ -13,6 +13,7 @@ type Coordinate struct {
 }
 
 var PROBLEM_MAP = make(map[string]int)
+var DUPS_FOUND = 0
 
 func main() {
 	input_file_name := "input.txt"
@@ -33,23 +34,33 @@ func main() {
 	}
 
 	// Grab the records and groups
-	var damaged_condition_records [][]byte
+	var damaged_condition_records []string
 	var partial_arrangement_groups [][]int
-	for _, line := range text {
+	for ind, line := range text {
 		split_row := strings.Split(line, " ")
 		if len(split_row) != 2 {
 			panic("Split row split weird")
 		}
-		damaged_condition_records = append(damaged_condition_records, []byte(split_row[0]))
+		damaged_condition_records = append(damaged_condition_records, split_row[0])
+		for i := 0; i < 4; i++ {
+			damaged_condition_records[ind] += "?"
+			damaged_condition_records[ind] += split_row[0]
+		}
 		split_condition_records := strings.Split(split_row[1], ",")
 		var row_partial_arrangement []int
-		for _, num_as_string := range split_condition_records {
-			temp_condition, err := strconv.Atoi(num_as_string)
-			if err != nil {
-				panic(err)
+		for i := 0; i < 5; i++ {
+			for _, num_as_string := range split_condition_records {
+				temp_condition, err := strconv.Atoi(num_as_string)
+				if err != nil {
+					panic(err)
+				}
+				row_partial_arrangement = append(row_partial_arrangement, temp_condition)
 			}
-			row_partial_arrangement = append(row_partial_arrangement, temp_condition)
 		}
+		for i := 0; i < len(row_partial_arrangement); i++ {
+			fmt.Printf("%d ", row_partial_arrangement[i])
+		}
+		println()
 		partial_arrangement_groups = append(partial_arrangement_groups, row_partial_arrangement)
 	}
 
@@ -60,13 +71,16 @@ func main() {
 		fmt.Printf("\n\n\n\nLine %s, Count: %d\n\n\n\n\n", damaged_condition_records[row_ind], new_count)
 		possible_count += new_count
 	}
+	fmt.Printf("Length of Memo: %d\n", len(PROBLEM_MAP))
+	fmt.Printf("Total dups found: %d\n", DUPS_FOUND)
 	fmt.Printf("Final arrangement count: %d\n", possible_count)
 }
 
-func Recursive_Row(parts_row []byte, valid_config_row []int) int {
+func Recursive_Row(parts_row string, valid_config_row []int) int {
 	problem_as_string := ProblemPartsToString(parts_row, valid_config_row)
 	val, ok := PROBLEM_MAP[problem_as_string]
 	if ok {
+		DUPS_FOUND++
 		return val
 	}
 	//println("Starting recursive row")
@@ -118,6 +132,6 @@ func Recursive_Row(parts_row []byte, valid_config_row []int) int {
 	return count
 }
 
-func ProblemPartsToString(parts_row []byte, valid_config_row []int) string {
-	return string(parts_row) + " " + strings.Trim(strings.Join(strings.Fields(fmt.Sprint(valid_config_row)), ","), "[]")
+func ProblemPartsToString(parts_row string, valid_config_row []int) string {
+	return parts_row + " " + strings.Trim(strings.Join(strings.Fields(fmt.Sprint(valid_config_row)), ","), "[]")
 }
