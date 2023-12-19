62e80e630e522198421a5e0a04292cfa3574fa5b
Keaton Brinkerhoff <keatonbrink@gmail.com>
2023-12-19 14:20:39 -0700

    Finish day 16 Part 2

--- AofC2023/day16/Part2/main.go
+++ AofC2023/day16/Part2/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 )
 
@@ -37,6 +36,48 @@ func main() {
 
 	mirror_layout := text
 
+	total_chan := 0
+
+	var path_length_chan = make(chan int, 100)
+	for i := 0; i < len(text[0]); i++ {
+
+		start_EOL := EndOfLight{Row: 0, Col: i, InputDirection: Up}
+
+		go SinglePath(path_length_chan, mirror_layout, start_EOL)
+
+		start_EOL = EndOfLight{Row: len(text) - 1, Col: i, InputDirection: Down}
+
+		go SinglePath(path_length_chan, mirror_layout, start_EOL)
+
+		total_chan += 2
+	}
+
+	for i := 0; i < len(text); i++ {
+
+		start_EOL := EndOfLight{Row: i, Col: 0, InputDirection: Left}
+
+		go SinglePath(path_length_chan, mirror_layout, start_EOL)
+
+		start_EOL = EndOfLight{Row: i, Col: len(text[0]) - 1, InputDirection: Down}
+
+		go SinglePath(path_length_chan, mirror_layout, start_EOL)
+
+		total_chan += 2
+	}
+
+	best_path_length := 0
+
+	for i := 0; i < total_chan; i++ {
+		ret_val := <-path_length_chan
+		if ret_val > best_path_length {
+			best_path_length = ret_val
+		}
+	}
+	println("Final ret val")
+	println(best_path_length)
+}
+
+func SinglePath(ret_chan chan<- int, mirror_layout []string, start_EOL EndOfLight) {
 	var visited_positions = make([][]bool, len(mirror_layout))
 
 	var visited_EOLS []EndOfLight
@@ -46,8 +87,6 @@ func main() {
 	}
 
 	var eol_positions []EndOfLight
-
-	start_EOL := EndOfLight{Row: 0, Col: 0, InputDirection: Left}
 
 	eol_positions = append(eol_positions, start_EOL)
 
@@ -126,9 +165,11 @@ func main() {
 		}
 	}
 
-	fmt.Printf("Final lens hash: %d\n", ret_val)
+	// fmt.Printf("Final lens hash: %d\n", ret_val)
+
+	ret_chan <- ret_val
 }
 
 func PrintEOL(eol EndOfLight) {
-	fmt.Printf("Row %d Col %d InputDirection %d\n", eol.Row, eol.Col, eol.InputDirection)
+	// fmt.Printf("Row %d Col %d InputDirection %d\n", eol.Row, eol.Col, eol.InputDirection)
 }
