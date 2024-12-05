224c912d4a523e01e09bff51c3340d4b418ed0dc
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-05 13:55:20 -0700

    d5 p1 Finished

--- AofC2024/d5/part1/main.go
+++ AofC2024/d5/part1/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 )
 
@@ -18,7 +20,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	is_blank := false
+	updates := []string{}
 
 	orderings := make(map[int]PageOrdering)
 	for _, line := range input {
@@ -31,13 +33,67 @@ func main() {
 			}
 			_, ok := orderings[first]
 			if !ok {
-				// orderings[first] = PageOrdering{previous: &[]int{second}}
-				// pageOrdering := orderings[first]
-				// *pageOrdering.future = append(*pageOrdering.future, second)
+				orderings[first] = PageOrdering{previous: &[]int{}, future: &[]int{}}
 			}
-
+			_, ok = orderings[second]
+			if !ok {
+				orderings[second] = PageOrdering{previous: &[]int{}, future: &[]int{}}
+			}
+			*orderings[first].future = append(*orderings[first].future, second)
+			*orderings[second].previous = append(*orderings[second].previous, first)
+		}
+		if strings.Contains(line, ",") {
+			updates = append(updates, line)
 		}
 	}
+
+	middle_page_sum := 0
+	for _, update := range updates {
+		pages := strings.Split(update, ",")
+		is_valid_ordering := true
+		for cur_page_index, page_string := range pages {
+			page, err := strconv.Atoi(page_string)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			for prev_update_page_index := 0; prev_update_page_index < cur_page_index; prev_update_page_index++ {
+				prev_update_page, err := strconv.Atoi(pages[prev_update_page_index])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				if slices.Contains(*orderings[page].future, prev_update_page) {
+					is_valid_ordering = false
+					break
+				}
+			}
+			for future_update_page_index := cur_page_index + 1; future_update_page_index < len(pages); future_update_page_index++ {
+				future_update_page, err := strconv.Atoi(pages[future_update_page_index])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				if slices.Contains(*orderings[page].previous, future_update_page) {
+					is_valid_ordering = false
+					break
+				}
+			}
+			if !is_valid_ordering {
+				break
+			}
+		}
+		if is_valid_ordering {
+			middle_page, err := strconv.Atoi(pages[len(pages)/2])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			middle_page_sum += middle_page
+		}
+	}
+
+	fmt.Println("Final: ", middle_page_sum)
 }
 
 func getInputAsLines() ([]string, error) {
