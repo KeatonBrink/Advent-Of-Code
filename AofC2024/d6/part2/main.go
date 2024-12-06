0d8fd14a235bfe54e4c9d2fb308ee5712eac9626
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-05 22:47:18 -0700

    d6 p2 initialize

--- AofC2024/d6/part2/main.go
+++ AofC2024/d6/part2/main.go
@@ -1,5 +1,7 @@
 package main
 
+// Go routines, probably a bad idea, but I need to try
+
 import (
 	"bufio"
 	"fmt"
@@ -17,6 +19,11 @@ const (
 type Person struct {
 	row       int
 	col       int
+	direction int
+}
+
+type VisitedSpot struct {
+	isVisited bool
 	direction int
 }
 
@@ -57,7 +64,9 @@ func main() {
 
 	lab_layout := input
 
+	run_count := 0
 	for !guard.isEnd(rows, cols) {
+		run_count++
 		if guard.direction == up {
 			if lab_layout[guard.row-1][guard.col] == '.' {
 				guard.row--
@@ -97,8 +106,11 @@ func main() {
 		}
 	}
 
-	fmt.Println("Final: ", visit_count)
+	fmt.Println("Rows, Cols: ", rows, cols)
+	fmt.Println("Final: ", visit_count, " Run Count: ", run_count)
 }
+
+// func testWorker
 
 func (p Person) isEnd(rows int, cols int) bool {
 	return (p.col == 0 && p.direction == left) || (p.row == 0 && p.direction == up) || (p.row == rows-1 && p.direction == down) || (p.col == cols-1 && p.direction == right)
