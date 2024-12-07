c4101aa080721e6d470dd54d058e3e23d8f41d13
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-06 20:28:20 -0700

    d6 p2 Finish with goroutines

--- AofC2024/d6/part2/main.go
+++ AofC2024/d6/part2/main.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"os"
 	"strings"
-	"sync"
 )
 
 const (
@@ -66,35 +65,74 @@ func main() {
 		}
 	}
 
-	is_loop_ch := make(chan bool)
+	is_loop_ch := make(chan bool, 100)
 
 	lab_layout := input
 
-	var wg sync.WaitGroup
+	go_routine_count := -1
+
+	for ri, row := range lab_layout {
+		for ci, elem := range row {
+			if elem == '.' && !(ri == guard.row && ci == guard.col) {
+				go_routine_count++
+				go runSimulation(lab_layout, guard, is_visited, ri, ci, is_loop_ch)
+			}
+		}
+	}
 
 	loop_count := 0
+
+	for go_routine_count > 0 {
+		go_routine_count--
+		if <-is_loop_ch {
+			loop_count++
+		}
+	}
 
 	fmt.Println("Rows, Cols: ", rows, cols)
 	fmt.Println("Final: ", loop_count)
 }
 
-func runSimulation(lab_layout_orig []string, guard_orig Person, is_visited_orig [][]VisitedSpot, obstruction_row, obstruction_col int, wg *sync.WaitGroup, is_loop_chan chan<- bool) {
-	defer (*wg).Done()
-	var lab_layout []string
-	copy(lab_layout, lab_layout_orig)
+func runSimulation(lab_layout_orig []string, guard_orig Person, is_visited_orig [][]VisitedSpot, obstruction_row, obstruction_col int, is_loop_chan chan<- bool) {
+	lab_layout := make([]string, len(lab_layout_orig))
+	for ri := range lab_layout_orig {
+		lab_layout[ri] = lab_layout_orig[ri]
+	}
 	rows := len(lab_layout)
 	cols := len(lab_layout[0])
 	guard := guard_orig
 	is_visited := make([][]VisitedSpot, rows)
-	for _, row := range rows {
+	for ri, row := range is_visited {
 		row = make([]VisitedSpot, cols)
-		for _, col :=
+		is_visited[ri] = make([]VisitedSpot, cols)
+		for ci := range row {
+			is_visited[ri][ci] = is_visited_orig[ri][ci]
+		}
 	}
-	copy(is_visited, is_visited_orig)
 
+	// for _, row := range is_visited {
+	// 	for _, col := range row {
+	// 		fmt.Printf("%v,", col)
+	// 	}
+	// 	fmt.Println()
+	// }
 
+	lab_layout[obstruction_row] = replaceAtIndex(lab_layout[obstruction_row], obstruction_col, '#')
+
+	steps := 0
 
 	for !guard.isEndOfLab(rows, cols) {
+		steps++
+		// fmt.Println("IN LOOP: ", obstruction_row, obstruction_col)
+		// fmt.Println(lab_layout)
+		// for _, row := range is_visited {
+		// 	for _, col := range row {
+		// 		fmt.Printf("%v,", col)
+		// 	}
+		// 	fmt.Println()
+		// }
+		// fmt.Println()
+		// fmt.Println()
 		if guard.direction == up {
 			if lab_layout[guard.row-1][guard.col] == '.' {
 				guard.row--
@@ -120,7 +158,15 @@ func runSimulation(lab_layout_orig []string, guard_orig Person, is_visited_orig 
 				guard.direction = up
 			}
 		}
+		// fmt.Println(guard)
+		// fmt.Println()
+		// fmt.Println()
 		visited_spot := is_visited[guard.row][guard.col]
+		if visited_spot.is_spot_visited == true && ((guard.direction == up && visited_spot.direction_up) || (guard.direction == right && visited_spot.direction_right) || (guard.direction == down && visited_spot.direction_down) || (guard.direction == left && visited_spot.direction_left)) {
+			// fmt.Println("Is loop: ", obstruction_row, obstruction_col, steps)
+			is_loop_chan <- true
+			return
+		}
 		visited_spot.is_spot_visited = true
 		switch guard.direction {
 		case up:
@@ -132,7 +178,20 @@ func runSimulation(lab_layout_orig []string, guard_orig Person, is_visited_orig 
 		case left:
 			visited_spot.direction_left = true
 		}
+		is_visited[guard.row][guard.col] = visited_spot
 	}
+
+	// fmt.Println("Is not Loop", obstruction_row, obstruction_col, steps)
+	is_loop_chan <- false
+	return
+}
+
+func replaceAtIndex(str string, index int, newChar rune) string {
+	runes := []rune(str)
+	if index >= 0 && index < len(runes) {
+		runes[index] = newChar
+	}
+	return string(runes)
 }
 
 // func testWorker
@@ -143,7 +202,7 @@ func (p Person) isEndOfLab(rows int, cols int) bool {
 
 func getInputAsLines() ([]string, error) {
 	// Read in files
-	f, err := os.Open("test.txt")
+	f, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
