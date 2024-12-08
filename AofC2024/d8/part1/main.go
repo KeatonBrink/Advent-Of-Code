43689e6c5766f11fb285c1bebf34a487fed9ba30
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-07 23:32:24 -0700

    d8 p1 Mistakenly allowed for any double length signal to work, but needs to be inline Will add slope to comparison

--- AofC2024/d8/part1/main.go
+++ AofC2024/d8/part1/main.go
@@ -3,8 +3,15 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 )
+
+// Thought process, one run to find all the possible towers
+// Then run through each spot again to find all the distances for each type of node, and search for a double
+// Not sure what the double looks like at this point, but probably just check if the double exists before adding
+
+// Note: map variables pertain to the data structure maps
 
 func main() {
 	input, err := getInputAsLines()
@@ -13,11 +20,89 @@ func main() {
 		return
 	}
 	fmt.Println(input)
+
+	city := input
+
+	blank_ant_distances := make(map[rune][]float64, 0)
+
+	for _, line := range city {
+		for _, ant := range line {
+			blank_ant_distances[ant] = make([]float64, 0)
+		}
+	}
+
+	spots_found := 0
+
+	// Lots of logic that should probably be broken down
+	// Cycle through every spot to look for distances
+	for ri, row := range city {
+		for ci := range row {
+			// for key, val := range blank_ant_distances {
+			// 	fmt.Println(key, val)
+			// }
+			// fmt.Println()
+			cur_ant_distance_map := copyAntDistanceMap(blank_ant_distances)
+			spot_found := false
+			// Compare current spot against all others
+			for cur_ri, cur_row := range city {
+				for cur_ci, cur_spot := range cur_row {
+					if cur_spot == '.' || (ri == cur_ri || ci == cur_ci) {
+						// Not antannae
+						continue
+					}
+					// Unnecessary, but feels clearier
+					cur_ant := cur_spot
+					dist := getDistanceBetweenPoints(ri, ci, cur_ri, cur_ci)
+					prev_ant_distances := cur_ant_distance_map[cur_ant]
+					for _, prev_ant_dist := range prev_ant_distances {
+						if dist*2 == prev_ant_dist || dist/2 == prev_ant_dist {
+							fmt.Println("ri1, ci1, ri2, ci2", ri, ci, cur_ri, cur_ci)
+							fmt.Println("Dist1, Dist2", dist, prev_ant_dist)
+							fmt.Println()
+							spots_found++
+							spot_found = true
+							break
+						}
+					}
+					if spot_found {
+						break
+					} else {
+						prev_ant_distances = append(prev_ant_distances, dist)
+						cur_ant_distance_map[cur_ant] = prev_ant_distances
+					}
+				}
+				if spot_found {
+					break
+				}
+			}
+			// for key, val := range cur_ant_distance_map {
+			// 	fmt.Println(key, val)
+			// }
+			// fmt.Println()
+		}
+	}
+
+	fmt.Println("Final: ", spots_found)
+}
+
+func getDistanceBetweenPoints(ri1, ci1, ri2, ci2 int) float64 {
+	dr := math.Pow(float64(ri1-ri2), 2)
+	dc := math.Pow(float64(ci1-ci2), 2)
+	return math.Sqrt(dr + dc)
+}
+
+func copyAntDistanceMap(og_map map[rune][]float64) map[rune][]float64 {
+	ret_map := make(map[rune][]float64, 0)
+	for key, value := range og_map {
+		ret_map[key] = make([]float64, len(value))
+		copy(ret_map[key], value)
+	}
+	return ret_map
 }
 
 func getInputAsLines() ([]string, error) {
 	// Read in files
-	f, err := os.Open("input.txt")
+	f, err := os.Open("test.txt")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
