cc6e10e829b3874fbf63055236cf39cec14fcc4d
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-10 07:59:57 -0700

    d10 p1 Initial work

--- AofC2024/d10/part1/main.go
+++ AofC2024/d10/part1/main.go
@@ -4,7 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sync"
 )
+
+const (
+	up    = iota
+	right = iota
+	down  = iota
+	left  = iota
+)
+
+type TrailPath struct {
+	head_x int
+	head_y int
+	peak_x int
+	peak_y int
+}
 
 func main() {
 	input, err := getInputAsLines()
@@ -13,6 +28,21 @@ func main() {
 		return
 	}
 	fmt.Println(input)
+
+	// This will be left unmodified
+	topo_map := make([][]int, len(input))
+
+	// Copy over ints to map
+	for _, input_line := range input {
+		topo_map_line := make([]int, len(input_line))
+		for _, elem := range input_line {
+			topo_map_line = append(topo_map_line, int(elem)-'0')
+		}
+	}
+
+}
+
+func moveOnMap(pos_x, pos_y int, topo_map [][]int, ch chan<- TrailPath, wg *sync.WaitGroup) {
 }
 
 func getInputAsLines() ([]string, error) {
