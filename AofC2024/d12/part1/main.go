bfb37f6bb2697b959188d568f27d6d35696edc0a
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-12 08:05:25 -0700

    d12 p1 Naive approach that doesn't work

--- AofC2024/d12/part1/main.go
+++ AofC2024/d12/part1/main.go
@@ -6,6 +6,11 @@ import (
 	"os"
 )
 
+type Plant struct {
+	perimeter int
+	area      int
+}
+
 func main() {
 	input, err := getInputAsLines()
 	if err != nil {
@@ -13,11 +18,53 @@ func main() {
 		return
 	}
 	fmt.Println(input)
+
+	plant_gardens := make(map[rune]Plant)
+
+	for _, line := range input {
+		for _, elem := range line {
+			_, ok := plant_gardens[elem]
+			if !ok {
+				plant_gardens[elem] = Plant{perimeter: 0, area: 0}
+			}
+		}
+	}
+
+	for ri, line := range input {
+		for ci, elem := range line {
+			temp_plant := plant_gardens[elem]
+			if ri == 0 || (rune(input[ri-1][ci]) != elem) {
+				temp_plant.perimeter++
+			}
+			if ci == len(line)-1 || (rune(input[ri][ci+1]) != elem) {
+				temp_plant.perimeter++
+			}
+			if ri == len(input)-1 || (rune(input[ri+1][ci]) != elem) {
+				temp_plant.perimeter++
+			}
+			if ci == 0 || (rune(input[ri][ci-1]) != elem) {
+				temp_plant.perimeter++
+			}
+			temp_plant.area++
+			plant_gardens[elem] = temp_plant
+		}
+	}
+
+	total_price := 0
+
+	for plant_name, plant_stats := range plant_gardens {
+		plant_price := plant_stats.area * plant_stats.perimeter
+		fmt.Println("Plant, perimeter, area, cost ", plant_name, plant_stats.perimeter, plant_stats.area, plant_price)
+		total_price += plant_price
+	}
+
+	fmt.Println("Final: ", total_price)
 }
 
 func getInputAsLines() ([]string, error) {
 	// Read in files
-	f, err := os.Open("input.txt")
+	f, err := os.Open("test.txt")
+	// f, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
