46a84064b349274e0a7ddc0ae3442fab8aed9864
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-13 22:08:38 -0700

    d13 p1 and p2 Finished

--- AofC2024/d13/part1/main.go
+++ AofC2024/d13/part1/main.go
@@ -61,21 +61,40 @@ func main() {
 			panic(err)
 		}
 		if n == 2 {
-			cur_claw_machine.Px = temp1
-			cur_claw_machine.Py = temp2
+			cur_claw_machine.Px = temp1 + 10000000000000
+			cur_claw_machine.Py = temp2 + 10000000000000
 			claw_machines = append(claw_machines, cur_claw_machine)
 			cur_claw_machine = ClawMachine{}
 		}
 	}
+
+	total_tokens := 0
 	for _, claw_machine := range claw_machines {
-		fmt.Println(claw_machine)
+		// x is the number of A inputs
+		// y is the number of B inputs
+		determinant := (claw_machine.Ax * claw_machine.By) - (claw_machine.Ay * claw_machine.Bx)
+		a_determinant := (claw_machine.Px * claw_machine.By) - (claw_machine.Bx * claw_machine.Py)
+		b_determinant := (claw_machine.Ax * claw_machine.Py) - (claw_machine.Px * claw_machine.Ay)
+
+		if determinant == 0 {
+			fmt.Println("This did not work: ", claw_machine)
+			continue
+		}
+		if a_determinant%determinant != 0 || b_determinant%determinant != 0 {
+			continue
+		}
+		a_input_count := a_determinant / determinant
+		b_input_count := b_determinant / determinant
+		total_tokens += (3 * a_input_count) + b_input_count
 	}
+
+	fmt.Println("Final: ", total_tokens)
 }
 
 func getInputAsLines() ([]string, error) {
 	// Read in files
-	f, err := os.Open("test.txt")
-	// f, err := os.Open("input.txt")
+	// f, err := os.Open("test.txt")
+	f, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return nil, err
