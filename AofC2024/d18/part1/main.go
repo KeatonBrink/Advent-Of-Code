0729eb694a817a3e9a3bff8547bdeac42ff51ba9
Keaton Brinkerhoff <keatonbrink@gmail.com>
2024-12-18 13:18:02 -0700

    d18 p1 and p2 finished

--- AofC2024/d18/part1/main.go
+++ AofC2024/d18/part1/main.go
@@ -4,7 +4,36 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
+
+const (
+	h    = 71
+	w    = 71
+	bits = 1024
+)
+
+// const (
+// 	h    = 7
+// 	w    = 7
+// 	bits = 12
+// )
+
+const (
+	r_bad_bit     = '#'
+	r_empty_space = '.'
+	r_person      = '@'
+)
+
+type Spot struct {
+	row int
+	col int
+}
+
+type Person struct {
+	spot     Spot
+	distance int
+}
 
 func main() {
 	input, err := getInputAsLines()
@@ -12,11 +41,124 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(input)
+	input_map := make([][]rune, h)
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			input_map[i] = append(input_map[i], r_empty_space)
+		}
+	}
+
+	for lc, line := range input {
+		if lc >= bits {
+			break
+		}
+		var col, row int
+		n, err := fmt.Sscanf(string(line), "%d,%d", &row, &col)
+		if err != nil {
+			panic(err)
+		} else if n != 2 {
+			panic("Not good")
+		}
+		input_map[row][col] = r_bad_bit
+	}
+
+	end := Spot{row: h - 1, col: w - 1}
+
+	var queue []Person
+	queue = append(queue, Person{})
+
+	var is_visited []Person
+
+	final_cost := 0
+
+	for len(queue) > 0 {
+		var cur_p Person
+		cur_p, queue = queue[0], queue[1:]
+
+		if isVisited(is_visited, cur_p) {
+			continue
+		}
+
+		is_visited = append(is_visited, cur_p)
+
+		// printGrid(input_map, cur_p)
+		// fmt.Println()
+		// time.Sleep(40 * time.Millisecond)
+
+		if cur_p.spot.col == end.col && cur_p.spot.row == end.row {
+			final_cost = cur_p.distance
+			break
+		}
+
+		if cur_p.spot.row > 0 && input_map[cur_p.spot.row-1][cur_p.spot.col] == r_empty_space {
+			new_p := Person{}
+			new_p.spot = cur_p.spot
+			new_p.distance = cur_p.distance + 1
+			new_p.spot.row--
+			// fmt.Println(new_p, cur_p)
+			queue = append(queue, new_p)
+		}
+		if cur_p.spot.row < h-1 && input_map[cur_p.spot.row+1][cur_p.spot.col] == r_empty_space {
+			new_p := Person{}
+			new_p.spot = cur_p.spot
+			new_p.distance = cur_p.distance + 1
+			new_p.spot.row++
+			// fmt.Println(new_p, cur_p)
+			queue = append(queue, new_p)
+		}
+		if cur_p.spot.col > 0 && input_map[cur_p.spot.row][cur_p.spot.col-1] == r_empty_space {
+			new_p := Person{}
+			new_p.spot = cur_p.spot
+			new_p.distance = cur_p.distance + 1
+			new_p.spot.col--
+			// fmt.Println(new_p, cur_p)
+			queue = append(queue, new_p)
+		}
+		if cur_p.spot.col < w-1 && input_map[cur_p.spot.row][cur_p.spot.col+1] == r_empty_space {
+			new_p := Person{}
+			new_p.spot = cur_p.spot
+			new_p.distance = cur_p.distance + 1
+			new_p.spot.col++
+			// fmt.Println(new_p, cur_p)
+			queue = append(queue, new_p)
+		}
+		sort.Slice(queue[:], func(i, j int) bool {
+			return queue[i].distance < queue[j].distance
+		})
+
+		// fmt.Println(queue)
+	}
+	fmt.Println("final_cost:", final_cost)
 }
 
-func getInputAsLines() ([]string, error) {
+func isVisited(ps []Person, p Person) bool {
+	for _, cur_p := range ps {
+		if cur_p.spot.row == p.spot.row && cur_p.spot.col == p.spot.col {
+			return true
+		}
+	}
+	return false
+}
+
+func printGrid(input [][]rune, p Person) {
+	for ri, line := range input {
+		for ci, elem := range line {
+			if elem == r_bad_bit {
+				fmt.Print(string(elem))
+			} else if p.spot.row == ri && p.spot.col == ci {
+				fmt.Print(string(r_person))
+			} else {
+				fmt.Print(string(r_empty_space))
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func getInputAsLines() ([][]rune, error) {
 	// Read in files
+	// f, err := os.Open("test2.txt")
+	// f, err := os.Open("test.txt")
 	f, err := os.Open("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -28,9 +170,9 @@ func getInputAsLines() ([]string, error) {
 	file_scanner.Split(bufio.ScanLines)
 
 	// Get lines
-	var text []string
+	var text [][]rune
 	for file_scanner.Scan() {
-		text = append(text, file_scanner.Text())
+		text = append(text, []rune(file_scanner.Text()))
 	}
 	return text, nil
 }
