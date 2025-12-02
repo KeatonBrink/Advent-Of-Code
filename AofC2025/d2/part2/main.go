53ef8c7dd7ca8cefa55905021dba529e297acd45
Keaton Brinkerhoff <keatonbrink@gmail.com>
2025-12-02 09:20:25 -0700

    d2: Finish both parts

--- AofC2025/d2/part2/main.go
+++ AofC2025/d2/part2/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -17,7 +19,57 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(input)
+	input = strings.Split(input[0], ",")
+	answer_set := make(map[int]bool)
+	for _, val := range input {
+		numbers := strings.Split(val, "-")
+		start_r, err := strconv.Atoi(numbers[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		end_r, err := strconv.Atoi(numbers[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		for test_val := start_r; test_val <= end_r; test_val += 1 {
+			test_val_string := strconv.Itoa(test_val)
+			// if len(test_val_string)%2 != 0 {
+			// 	continue
+			// }
+			ind1 := 0
+			for ind2 := 0; ind2 < len(test_val_string)/2; ind2 += 1 {
+				sub_str := test_val_string[ind1 : ind2+1]
+				fmt.Println(sub_str)
+				if len(test_val_string)%len(sub_str) != 0 {
+					continue
+				}
+				is_valid := true
+				for mult := 1; mult*len(sub_str)+ind2 < len(test_val_string); mult += 1 {
+					test_sub := test_val_string[mult*len(sub_str) : ind2+mult*len(sub_str)+1]
+					if sub_str != test_sub {
+						is_valid = false
+					}
+				}
+				if is_valid {
+					fmt.Println("Is Valid", test_val, start_r, end_r)
+					answer_set[test_val] = true
+				}
+			}
+		}
+	}
+	total := 0
+	for key, _ := range answer_set {
+		total += key
+	}
+
+	fmt.Println(total)
+}
+
+func countDigitsString(num int) int {
+	return len(strconv.Itoa(num))
 }
 
 func getInputAsLines(file_name string) ([]string, error) {
