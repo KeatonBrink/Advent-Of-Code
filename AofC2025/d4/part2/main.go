7ebb12690c3d5c16bf4f071ad189b425995d8692
Keaton Brinkerhoff <keatonbrink@gmail.com>
2025-12-04 10:16:42 -0700

    Finish d4 p2

--- AofC2025/d4/part2/main.go
+++ AofC2025/d4/part2/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -17,7 +18,76 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(input)
+	input = addBuffer(input, '.')
+	roll_char := '@'
+	total := 0
+	is_roll_moved := true
+	for is_roll_moved {
+		is_roll_moved = false
+		next_set := make([]string, len(input))
+		copy(next_set, input)
+
+		for line_ind := 1; line_ind < len(input)-1; line_ind += 1 {
+			line := input[line_ind]
+			for char_ind := 1; char_ind < len(line)-1; char_ind += 1 {
+				roll_count := 0
+				char := line[char_ind]
+				if char != byte(roll_char) {
+					continue
+				}
+				roll_count += addIfRoll(input, line_ind-1, char_ind-1, roll_char)
+				roll_count += addIfRoll(input, line_ind-1, char_ind, roll_char)
+				roll_count += addIfRoll(input, line_ind-1, char_ind+1, roll_char)
+				roll_count += addIfRoll(input, line_ind, char_ind-1, roll_char)
+				roll_count += addIfRoll(input, line_ind, char_ind+1, roll_char)
+				roll_count += addIfRoll(input, line_ind+1, char_ind-1, roll_char)
+				roll_count += addIfRoll(input, line_ind+1, char_ind, roll_char)
+				roll_count += addIfRoll(input, line_ind+1, char_ind+1, roll_char)
+
+				if roll_count < 4 {
+					// fmt.Println(line_ind, char_ind, input[line_ind][char_ind])
+					// fmt.Println()
+					next_set = replaceByteInStringSlice(next_set, line_ind, char_ind, 'X')
+					is_roll_moved = true
+					total += 1
+				}
+
+			}
+		}
+		input = next_set
+	}
+	fmt.Println(total)
+}
+
+func replaceByteInStringSlice(input []string, line_ind, char_ind int, new_char rune) []string {
+	output := make([]string, len(input))
+	copy(output, input)
+	new_str := []rune(output[line_ind])
+	new_str[char_ind] = new_char
+	output[line_ind] = string(new_str)
+	return output
+}
+
+func addIfRoll(input []string, line_ind, char_ind int, roll_char rune) int {
+	if input[line_ind][char_ind] == byte(roll_char) {
+		// fmt.Println(line_ind, char_ind)
+		return 1
+	}
+	return 0
+}
+
+func addBuffer(input []string, buffer_char rune) []string {
+	output := make([]string, len(input))
+	copy(output, input)
+	for ind, line := range output {
+		output[ind] = string(buffer_char) + line + string(buffer_char)
+	}
+	str_length := len(input[0]) + 2
+	new_str := strings.Repeat(string(buffer_char), str_length)
+	output = append(output, new_str)
+	output = append([]string{new_str}, output...)
+
+	return output
 }
 
 func getInputAsLines(file_name string) ([]string, error) {
