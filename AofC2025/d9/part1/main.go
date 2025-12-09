9791febd1758d96c3b5e97a446d63ba87c336637
Keaton Brinkerhoff <keatonbrink@gmail.com>
2025-12-09 08:38:06 -0700

    d9 p1 finished

--- AofC2025/d9/part1/main.go
+++ AofC2025/d9/part1/main.go
@@ -3,8 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type TileCorner struct {
+	row int
+	col int
+}
+
+func (tc1 TileCorner) GetArea(tc2 TileCorner) int {
+	return (int(math.Abs((float64(tc1.row - tc2.row)))) + 1) * (int(math.Abs(float64(tc1.col-tc2.col))) + 1)
+}
 
 func main() {
 	file_name := "input.txt"
@@ -17,7 +29,33 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(input)
+	var tile_corners []TileCorner
+	for _, line := range input {
+		parts := strings.Split(line, ",")
+		ind1, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		ind2, err := strconv.Atoi(parts[1])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		cur_tile_corner := TileCorner{ind1, ind2}
+		tile_corners = append(tile_corners, cur_tile_corner)
+
+	}
+	largest_area := 0
+	for tile_corner1_ind := 0; tile_corner1_ind < len(tile_corners)-1; tile_corner1_ind++ {
+		for tile_corner2_ind := tile_corner1_ind + 1; tile_corner2_ind < len(tile_corners); tile_corner2_ind++ {
+			cur_area := tile_corners[tile_corner1_ind].GetArea(tile_corners[tile_corner2_ind])
+			if cur_area > largest_area {
+				largest_area = cur_area
+			}
+		}
+	}
+	fmt.Println(largest_area)
 }
 
 func getInputAsLines(file_name string) ([]string, error) {
