ea8fb1df8269210f12afb193bc31eb2c0d647204
Keaton Brinkerhoff <keatonbrink@gmail.com>
2025-12-11 07:17:14 -0700

    d11 p1 finished

--- AofC2025/d11/part1/main.go
+++ AofC2025/d11/part1/main.go
@@ -4,7 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
+
+type Path struct {
+	path []string
+}
 
 func main() {
 	file_name := "input.txt"
@@ -17,7 +23,42 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Println(input)
+	// fmt.Println(input)
+	connection_dict := make(map[string][]string)
+
+	for _, line := range input {
+		line_fields := strings.Fields(line)
+		home := line_fields[0]
+		home = home[:len(home)-1]
+		outgoing := line_fields[1:]
+		connection_dict[home] = outgoing
+	}
+
+	total_paths := 0
+	var paths []Path
+	paths = append(paths, Path{[]string{"you"}})
+	for len(paths) > 0 {
+		var popped_path Path
+		popped_path, paths = paths[0], paths[1:]
+		cur_path := popped_path.path
+		cur_loc := cur_path[len(cur_path)-1]
+		potential_next_paths := connection_dict[cur_loc]
+		for _, potential_next_path_add := range potential_next_paths {
+			// Avoid circles
+			if slices.Contains(cur_path, potential_next_path_add) {
+				continue
+			} else if potential_next_path_add == "out" {
+				total_paths++
+				continue
+			}
+			new_path := make([]string, len(cur_path)+1)
+			copy(new_path, cur_path)
+			new_path[len(new_path)-1] = potential_next_path_add
+			paths = append(paths, Path{new_path})
+		}
+	}
+
+	fmt.Println(total_paths)
 }
 
 func getInputAsLines(file_name string) ([]string, error) {
